275c5661608c90f3ec3706cfcb9412b18681870b
akmitrich <ak.mitrich@mail.ru>
2022-06-30 00:13:23 +0300

    Boundary wrap

--- mol_job/src/boundaries.rs
+++ mol_job/src/boundaries.rs
@@ -1,5 +1,7 @@
 #![allow(unused, dead_code)]
 
+use std::ops::AddAssign;
+
 use d_vector::{DVector, Real};
 
 #[derive(Debug)]
@@ -10,5 +12,38 @@ pub struct Region<const D: usize> {
 impl<const D: usize> Region<D> {
     pub fn new(size: [Real; D]) -> Self {
         Self { inner: DVector::from(size) }
+    }
+
+    pub fn wrap(&self, position: &mut DVector<D>) {
+        let mut shift = [0 as Real; D];
+        for (i, s) in shift.iter_mut().enumerate() {
+            if self.is_above(position, i) {
+                s.add_assign(-self.inner.components()[i]);
+            } else if self.is_below(position, i) {
+                s.add_assign(self.inner.components()[i]);
+            }
+        }
+        position.add_assign(DVector::from(shift));
+    }
+
+    fn is_above(&self, position: &DVector<D>, index: usize) -> bool {
+        position.components()[i] >= self.inner.components()[i] / 2.
+    }
+
+    fn is_below(&self, position: &DVector<D>, index: usize) -> bool {
+        position.components()[i] < -self.inner.components()[i] / 2.
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wrap() {
+        let region = Region::new([1., 5.]);
+        let mut p = DVector::from([1.5, -4.]);
+        region.wrap(&mut p);
+        assert_eq!(&[0.5, 1.], p.components());
     }
 }
